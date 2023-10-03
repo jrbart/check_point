83b15bae2f53c14860d526c0d4e4c018fd04ad06
jrbart <jorabart@gmail.com>
2023-10-03 10:20:42 -0500

    Added mix_test_watch for some TDD fun

--- mix.exs
+++ mix.exs
@@ -43,7 +43,8 @@ defmodule CheckPoint.MixProject do
       {:telemetry_poller, "~> 1.0"},
       {:gettext, "~> 0.20"},
       {:jason, "~> 1.2"},
-      {:plug_cowboy, "~> 2.5"}
+      {:plug_cowboy, "~> 2.5"},
+      {:mix_test_watch,"~> 1.0"}
     ]
   end
 
